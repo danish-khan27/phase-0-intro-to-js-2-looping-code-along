24cd4dffc81baee5151c9f5b13438a99917c07b8
danish-khan27 <dkhans2001@gmail.com>
2025-01-21 02:45:54 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+// Task 1: Function to generate thank-you messages using a for loop
+function writeCards(names, event) {
+    let messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+  
+    return messages;
+  }
+  
+  // Task 2: Function to count down to zero using a while loop
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+
+  
